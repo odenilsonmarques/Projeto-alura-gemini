5aec3e86ab89f57aab1999bc0dc3aaab57b95fd1
odenilson marques <odmarquesgm@gmail.com>
2024-09-04 11:29:13 -0300

    create file to insert the datas

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+
+// Exibindo o valor da VARIAVEL declarada no arquivo dados-atletas
+console.log(dados)
+
+// Exibindo o valores da LISTA declarada no arquivo dados-atletas
+console.log(lista_dados)
+console.log(lista_dados[1])//Exibindo um item da lista atraves do seu indice
+
+
+//Exibindo as informaçlões do OBJETOS declarado no arquivo dados-atletas
+console.log(objetos_dados)
+console.log(objetos_dados[0].titulo)
+
+
+
---
+++ dados-atletas.js
@@ -0,0 +1,33 @@
+// Dessa forma estou usando VARIAVEL para atribuir o valor
+let dados = "rayssa leal"
+
+
+// Dessa forma estou usando LISTA para declarar os valores
+let lista_dados = [
+    "Rayssa Leal",
+    "Rebea Andrade",
+    "Beatriz Souza"
+];
+
+
+// Dessa forma estou usando OBJETOS para declarar as informaçẽos 
+
+let objetos_dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "é uma skatista brasileira que, com apenas 13 anos, conquistou o mundo ao se tornar a mais jovem medalhista olímpica da história do Brasil. Nascida em Imperatriz, no Maranhão, Raíssa é conhecida por sua técnica apurada e estilo único",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "é uma ginasta artística brasileira que fez história ao se tornar a maior medalhista olímpica do Brasil. Nascida em Guarulhos, em 1999, Rebeca conquistou o coração dos brasileiros com sua determinação, talento e carisma.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    
+    {
+        titulo : "Beatriz Souza",
+        descricao : " é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua força, técnica e determinação a tornaram uma das principais atletas do judô brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+]
