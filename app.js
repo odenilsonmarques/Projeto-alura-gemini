b1bec86f44b6d45f9db637a739d993715c163bca
odenilson marques <odmarquesgm@gmail.com>
2024-09-06 18:08:46 -0300

    aplicate logic to search

--- app.js
+++ app.js
@@ -2,67 +2,75 @@ function buscar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
     let section_resultado = document.getElementById("resultados-pesquisa")
 
+    let campoBusca = document.getElementById("campo-busca").value
+    
+    if(!campoBusca){
+        alert('campo de pesquisa vazio. Por favor preencha o campo')
+       return
+    }
+
     // Inicializa uma string vazia para armazenar os resultados formatados
-    let resultado_cursos = ""
+    let resultadoEstados = ""
 
-    // Itera sobre cada curso na lista de cursos
-    for (let curso of cursos) {
+    // Itera sobre cada estado na lista de estados
+    for (let estado of estados) {
 
-        // Cria uma div para cada curso, formatando os dados do curso em HTML
-        resultado_cursos += `
+        if (estado.nome.includes(campoBusca)) {
 
-        <div class="item-resultado">
-            <h2>${curso.nome}</h2>
+            // Cria uma div para cada estado, formatando os dados do estado em HTML
+            resultadoEstados += `
 
-            <table style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Formação</th>
-                        <th>Duração</th>
-                        <th>Modalidade</th>
-                        <th>Modalidade Online</th>
-                        <th>Modalidade Presencial</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>${curso.formacao}</td>
-                        <td>${curso.duracao}</td>
-                        <td>${curso.modalidade}</td>
-                        <td>
-                            <a href="${curso.link_online}"
-                                target="_blank"><strong>Saber mais</strong>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="${curso.link_presencial}"
-                                target="_blank"><strong>Saber mais</strong>
-                            </a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <p class="descricao-meta">
-                ${curso.descricao}
-            </p>
-            <ul class="lista-horizontal">
-                <li>
-                    <a href="https://www.instagram.com/rebecarandrade/" target="_blank" title="Instagram"><img
-                            src="img/facebook.png" alt="Instagram" >
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/watch?v=U6QlKjfTSpc" target="_blank" title="YouTube"><img
-                            src="img/twitter.png" alt="YouTube">
-                    </a>
-                </li> 
-            </ul>
-            <a href="https://www.fiap.com.br/" target="_blank"><strong><span class="fiap">FIAP</span> - ALURA</strong></a>
-        </div>
-        `
+            <div class="item-resultado">
+                <h2>${estado.nome}</h2>
+
+                <div style="display: flex; align-items: flex-start;">
+                    <table style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Capital</th>
+                                <td>${estado.capital}</td>
+                            </tr> 
+                            <tr>
+                                <th>Área Territorial</th>
+                                <td>${estado.areaTerritorial.toLocaleString('pt-BR')} km²</td>
+                            </tr> 
+                            <tr>
+                                <th>População</th>
+                                <td>${estado.populacao.toLocaleString('pt-BR')} habitantes</td>
+                            </tr>
+                            <tr>
+                                <th>Densidade Demográfica</th>
+                                <td>${estado.densidadeDemografica} hab/km²</td>
+                            </tr>
+                            <tr>
+                                <th>Região</th>
+                                <td>${estado.regiao}</td>
+                            </tr> 
+                            <tr>
+                                <th>IDH</th>
+                                <td>${estado.idh}</td>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+                <p class="descricao-meta">
+                    ${estado.descricao}
+                </p>
+                <ul class="lista-horizontal">
+                    <li>
+                        <a href="${estado.map}" target="_blank" title="Instagram"><img
+                                src="img/fixar-mapa.png" alt="Instagram" >
+                        </a>
+                    </li>
+                </ul>
+                <a href="${estado.link}" target="_blank"><strong><span>saiba mais</strong></a>
+            </div>
+                    `
+        }
     }
     // Atribui o HTML gerado à seção de resultados
-    section_resultado.innerHTML = resultado_cursos
+    section_resultado.innerHTML = resultadoEstados
 }
 
 
